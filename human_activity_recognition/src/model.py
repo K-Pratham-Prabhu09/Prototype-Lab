Update training configuration and improve model training logic; set EPOCHS to 60, adjust train_one_epoch to calculate average loss correctly, and fix output indexing in MyNeuralNetwork.

human_activity_recognition/src/model.py
@@ -12,7 +12,7 @@ class MyNeuralNetwork(nn.Module):
         
     def forward(self,x):
         out, _ = self.lstm(x)
-        out = self.fc(out[-1,:])
+        out = self.fc(out[:,-1,:])
         return out
     
 